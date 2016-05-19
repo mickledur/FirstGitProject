bf8b5d5af7444f6e452a53a73d6bc450e652d774
mickledur <michael.zodehougan@gmail.com>
2016-05-19 15:09:40 +0200

    Firstcommit

---
+++ src/firstgitproject/Calculator.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package firstgitproject;
+
+/**
+ *
+ * @author geekzone
+ */
+public class Calculator {
+    
+    public static void somme(int num1, int num2){
+        
+        int sum=num1+num2;
+        System.out.println(num1+" + "+num2+" = "+sum);
+    }
+    
+}
