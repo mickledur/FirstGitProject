9da4a6e3dd2beb2996e5fc3365fc4d690a0fe869
mickledur <michael.zodehougan@gmail.com>
2016-05-19 21:05:43 +0200

    Adding diff function

--- src/firstgitproject/Calculator.java
+++ src/firstgitproject/Calculator.java
@@ -11,10 +11,16 @@ package firstgitproject;
  */
 public class Calculator {
     
-    public static void somme(int num1, int num2){
+    public static void sum(int num1, int num2){
         
         int sum=num1+num2;
         System.out.println(num1+" + "+num2+" = "+sum);
+    }
+    
+     public static void diff(int num1, int num2){
+        
+        int diif=num1-num2;
+        System.out.println(num1+" + "+num2+" = "+diif);
     }
     
 }
--- src/firstgitproject/FirstGitProject.java
+++ src/firstgitproject/FirstGitProject.java
@@ -16,7 +16,7 @@ public class FirstGitProject {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Calculator.somme(10, 15);
+        Calculator.sum(10, 15);
     }
     
 }
